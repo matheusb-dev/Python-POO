3851912fff086a2e65512ef4b094bdc0defcc147
matheusb-dev <leitinhogames6@gmail.com>
2025-08-13 20:12:01 -0300

    operadores

---
+++ calculadora.py
@@ -0,0 +1,18 @@
+numero_1 = 90
+numero_2 = 50
+
+soma = numero_1 + numero_2
+subtracao = numero_1 - numero_2
+multiplicacao = numero_1 * numero_2
+divisao = numero_1 / numero_2
+divisao_inteira = numero_1 // numero_2
+modulo = numero_1 % numero_2
+exponenciacaao = numero_1 ** numero_2
+
+print("Soma: ", soma) 
+print("Substracao: ", subtracao) 
+print("Multiplicacao: ", multiplicacao) 
+print("Divisao: ", divisao) 
+print("Divisao Inteira: ", divisao_inteira) 
+print("Modulo: ", modulo) 
+print("Esponenciacao: ", exponenciacaao) 
